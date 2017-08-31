b4d630f8137d5042f7ccea4a6e1ace58a8c7961d
Corentin Henry <corentinhenry@gmail.com>
2017-08-31 13:26:19 -0700

    convert hard tabs to soft tabs

--- books_database_oop/backend.py
+++ books_database_oop/backend.py
@@ -1,37 +1,38 @@
 import sqlite3
 
+
 class Database:
 
-	def __init__(self, database):
-		self.con=sqlite3.connect("books.db")
-		self.cur=self.con.cursor()
-		self.cur.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRiMARY KEY , title TEXT, author TEXT, year INTEGER, country TEXT, borrowed TEXT)")
-		self.con.commit()
+    def __init__(self, database):
+        self.con=sqlite3.connect("books.db")
+        self.cur=self.con.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRiMARY KEY , title TEXT, author TEXT, year INTEGER, country TEXT, borrowed TEXT)")
+        self.con.commit()
 
-	def insert(self, title, author, year, country, borrowed):
-		self.cur.execute("INSERT INTO books VALUES(NULL,?,?,?,?,?)",(title, author, year, country, borrowed))
-		self.con.commit()
+    def insert(self, title, author, year, country, borrowed):
+        self.cur.execute("INSERT INTO books VALUES(NULL,?,?,?,?,?)",(title, author, year, country, borrowed))
+        self.con.commit()
 
-	def view(self):
-		self.cur.execute("SELECT * FROM books")
-		rows=self.cur.fetchall()
-		return rows
+    def view(self):
+        self.cur.execute("SELECT * FROM books")
+        rows=self.cur.fetchall()
+        return rows
 
-	def search(self, title="", author="", year="", country="", borrowed=""):
-		self.cur.execute("SELECT * FROM books WHERE title=? OR author=? OR year=? OR country=? OR borrowed=?",(title, author, year, country, borrowed))
-		rows=self.cur.fetchall()
-		return rows
+    def search(self, title="", author="", year="", country="", borrowed=""):
+        self.cur.execute("SELECT * FROM books WHERE title=? OR author=? OR year=? OR country=? OR borrowed=?",(title, author, year, country, borrowed))
+        rows=self.cur.fetchall()
+        return rows
 
-	def delete(self, id):
-		self.cur.execute("DELETE FROM books WHERE id=?",(id,))
-		self.con.commit()
+    def delete(self, id):
+        self.cur.execute("DELETE FROM books WHERE id=?",(id,))
+        self.con.commit()
 
-	def update(self, id, title, author, year, country, borrowed):
-		self.cur.execute("UPDATE books SET  title=?, author=?, year=?, country=?, borrowed=? WHERE id=?",(title, author, year, country, borrowed, id))
-		self.con.commit()
+    def update(self, id, title, author, year, country, borrowed):
+        self.cur.execute("UPDATE books SET  title=?, author=?, year=?, country=?, borrowed=? WHERE id=?",(title, author, year, country, borrowed, id))
+        self.con.commit()
 
-	def __del__(self):
-		self.con.close()
+    def __del__(self):
+        self.con.close()
 
 
 
--- books_database_oop/personal_bookstore.py
+++ books_database_oop/personal_bookstore.py
@@ -5,123 +5,123 @@ database = Database("books.db")
 
 class Window:
 
-	def __init__(self, window):
+    def __init__(self, window):
 
-		self.window=window
+        self.window=window
 
-		self.window.wm_title("My Books")
+        self.window.wm_title("My Books")
 
-		# title
-		label_title=Label(window, text="Title")
-		label_title.grid(row=0, column=0)
+        # title
+        label_title=Label(window, text="Title")
+        label_title.grid(row=0, column=0)
 
-		self.title=StringVar()
-		self.e_title=Entry(window, textvariable=self.title)
-		self.e_title.grid(row=0,column=1)
+        self.title=StringVar()
+        self.e_title=Entry(window, textvariable=self.title)
+        self.e_title.grid(row=0,column=1)
 
-		# author
-		label_author=Label(window, text="Author")
-		label_author.grid(row=0, column=2)
+        # author
+        label_author=Label(window, text="Author")
+        label_author.grid(row=0, column=2)
 
-		self.author=StringVar()
-		self.e_author=Entry(window, textvariable=self.author)
-		self.e_author.grid(row=0,column=3)
+        self.author=StringVar()
+        self.e_author=Entry(window, textvariable=self.author)
+        self.e_author.grid(row=0,column=3)
 
-		# year
-		label_year=Label(window, text="Year")
-		label_year.grid(row=1, column=0)
+        # year
+        label_year=Label(window, text="Year")
+        label_year.grid(row=1, column=0)
 
-		self.year=StringVar()
-		self.e_year=Entry(window, textvariable=self.year)
-		self.e_year.grid(row=1,column=1)
+        self.year=StringVar()
+        self.e_year=Entry(window, textvariable=self.year)
+        self.e_year.grid(row=1,column=1)
 
-		# country
-		label_country=Label(window, text="Country")
-		label_country.grid(row=1, column=2)
+        # country
+        label_country=Label(window, text="Country")
+        label_country.grid(row=1, column=2)
 
-		self.country=StringVar()
-		self.e_country=Entry(window, textvariable=self.country)
-		self.e_country.grid(row=1,column=3)
+        self.country=StringVar()
+        self.e_country=Entry(window, textvariable=self.country)
+        self.e_country.grid(row=1,column=3)
 
-		# borrowed
-		label_borrowed=Label(window, text="Borrowed")
-		label_borrowed.grid(row=2, column=0)
+        # borrowed
+        label_borrowed=Label(window, text="Borrowed")
+        label_borrowed.grid(row=2, column=0)
 
-		self.borrowed=StringVar()
-		self.e_borrowed=Entry(window, textvariable=self.borrowed)
-		self.e_borrowed.grid(row=2,column=1)
+        self.borrowed=StringVar()
+        self.e_borrowed=Entry(window, textvariable=self.borrowed)
+        self.e_borrowed.grid(row=2,column=1)
 
-		# list box and attached scroll bar
-		self.listbox=Listbox(window, height=10, width=40)
-		self.listbox.grid(row=3, column=0, rowspan=6, columnspan=3)
-		# get_selected_row function is triggered when user select an item in the listbox
-		self.listbox.bind("<<ListboxSelect>>", self.get_selected_row)
+        # list box and attached scroll bar
+        self.listbox=Listbox(window, height=10, width=40)
+        self.listbox.grid(row=3, column=0, rowspan=6, columnspan=3)
+        # get_selected_row function is triggered when user select an item in the listbox
+        self.listbox.bind("<<ListboxSelect>>", self.get_selected_row)
 
-		# scrollbar=Scrollbar(window)
-		# scrollbar.grid(row=2,column=2,rowspan=6)
+        # scrollbar=Scrollbar(window)
+        # scrollbar.grid(row=2,column=2,rowspan=6)
 
-		# listbox.configure(yscrollcommand=scrollbar.set)
-		# scrollbar.configure(command=listbox.yview)
+        # listbox.configure(yscrollcommand=scrollbar.set)
+        # scrollbar.configure(command=listbox.yview)
 
-		# buttons
+        # buttons
 
-		b_view=Button(window, text="View all", width=12, command=self.view_command)
-		b_view.grid(row=3, column=3)
+        b_view=Button(window, text="View all", width=12, command=self.view_command)
+        b_view.grid(row=3, column=3)
 
-		b_search=Button(window, text="Search", width=12, command=self.search_command)
-		b_search.grid(row=4, column=3)
+        b_search=Button(window, text="Search", width=12, command=self.search_command)
+        b_search.grid(row=4, column=3)
 
-		b_add=Button(window, text="Add", width=12, command=self.add_command)
-		b_add.grid(row=5, column=3)
+        b_add=Button(window, text="Add", width=12, command=self.add_command)
+        b_add.grid(row=5, column=3)
 
-		b_update=Button(window, text="Update", width=12, command=self.update_command)
-		b_update.grid(row=6, column=3)
+        b_update=Button(window, text="Update", width=12, command=self.update_command)
+        b_update.grid(row=6, column=3)
 
-		b_delete=Button(window, text="Delete", width=12, command=self.delete_command)
-		b_delete.grid(row=7, column=3)
+        b_delete=Button(window, text="Delete", width=12, command=self.delete_command)
+        b_delete.grid(row=7, column=3)
 
-	def view_command(self):
-		self.listbox.delete(0,END)
-		for row in database.view():
-			self.listbox.insert(END, row)
+    def view_command(self):
+        self.listbox.delete(0,END)
+        for row in database.view():
+            self.listbox.insert(END, row)
 
-	def search_command(self):
-		self.listbox.delete(0,END)
-		for row in database.search(self.title.get(), self.author.get(), self.year.get(), self.country.get(), self.borrowed.get()):
-			self.listbox.insert(END, row)
+    def search_command(self):
+        self.listbox.delete(0,END)
+        for row in database.search(self.title.get(), self.author.get(), self.year.get(), self.country.get(), self.borrowed.get()):
+            self.listbox.insert(END, row)
 
-	def add_command(self):
-		database.insert(self.title.get(), self.author.get(), self.year.get(), self.country.get(), self.borrowed.get())
-		self.listbox.delete(0,END)
-		self.listbox.insert(END, (self.title.get(), self.author.get(), self.year.get(), self.country.get(), self.borrowed.get()))
+    def add_command(self):
+        database.insert(self.title.get(), self.author.get(), self.year.get(), self.country.get(), self.borrowed.get())
+        self.listbox.delete(0,END)
+        self.listbox.insert(END, (self.title.get(), self.author.get(), self.year.get(), self.country.get(), self.borrowed.get()))
 
-	def get_selected_row(self, event):
-		index=self.listbox.curselection()[0] #return index of the row in form of a tuple (index,) hence the [0]
-		self.selected_tuple=listbox.get(index)
-		#fill entries with selected row
-		self.e_title.delete(0, END)
-		self.e_title.insert(END, self.selected_tuple[1])
-		self.e_author.delete(0, END)
-		self.e_author.insert(END, self.selected_tuple[2])
-		self.e_year.delete(0,END)
-		self.e_year.insert(END, self.selected_tuple[3])
-		self.e_country.delete(0, END)
-		self.e_country.insert(END, self.selected_tuple[4])
-		self.e_borrowed.delete(0, END)
-		self.e_borrowed.insert(END, self.selected_tuple[5])
+    def get_selected_row(self, event):
+        index=self.listbox.curselection()[0] #return index of the row in form of a tuple (index,) hence the [0]
+        self.selected_tuple=listbox.get(index)
+        #fill entries with selected row
+        self.e_title.delete(0, END)
+        self.e_title.insert(END, self.selected_tuple[1])
+        self.e_author.delete(0, END)
+        self.e_author.insert(END, self.selected_tuple[2])
+        self.e_year.delete(0,END)
+        self.e_year.insert(END, self.selected_tuple[3])
+        self.e_country.delete(0, END)
+        self.e_country.insert(END, self.selected_tuple[4])
+        self.e_borrowed.delete(0, END)
+        self.e_borrowed.insert(END, self.selected_tuple[5])
 
-	def delete_command(self):
-		database.delete(selected_tuple[0])
-		self.listbox.delete(ANCHOR)
-		self.e_title.delete(0,END)
-		self.e_author.delete(0,END)
-		self.e_year.delete(0,END)
-		self.e_country.delete(0,END)
-		self.e_borrowed.delete(0,END)
+    def delete_command(self):
+        database.delete(selected_tuple[0])
+        self.listbox.delete(ANCHOR)
+        self.e_title.delete(0,END)
+        self.e_author.delete(0,END)
+        self.e_year.delete(0,END)
+        self.e_country.delete(0,END)
+        self.e_borrowed.delete(0,END)
 
-	def update_command(self):
-		database.update(self.selected_tuple[0], self.title.get(), self.author.get(), self.year.get(), self.country.get(), self.borrowed.get())
-		view_command()
+    def update_command(self):
+        database.update(self.selected_tuple[0], self.title.get(), self.author.get(), self.year.get(), self.country.get(), self.borrowed.get())
+        view_command()
 
 
 window = Tk()
